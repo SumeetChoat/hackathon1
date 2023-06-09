d01d5d2ad4f38a1c420f2a9c31ad99e40ede67ac
“SumeetChoat” <sumeetpalchoat@hotmail.co.uk>
2023-06-09 10:10:58 +0100

    added computerInput for RPS game

--- hackathon1.js
+++ hackathon1.js
@@ -6,6 +6,8 @@
 // - Paper beats rock
 // - Tie game = game ends in draw
 
+
+// 1) get player choice
 const getUserChoice = (userInput) => {
     userInput = userInput.toLowerCase();
     if (userInput === 'rock' | userInput === 'paper' | userInput === 'scissors') {
@@ -14,4 +16,27 @@ const getUserChoice = (userInput) => {
         console.log('Error, please enter valid response: rock, paper or scossors');
     }
 }
+
+//2) get computer choice(funny because they can't think it's just randomised)
+
+const getComputerChoice = () => {
+    let randNum = math.random()*3;
+    return randNum === 0 ?
+    'rock' : (randNum === 1 ?
+    'paper' : 'scissors');
+        switch (randNum) {
+            case 0:
+                return 'rock';
+                break;
+                case 1:
+                    return 'paper';
+                    break;
+                    case 2:
+                        return 'scissors';
+                        break;
+                        default:
+                            console.log('Try again!');
+                            break;
+        }
+};
 
