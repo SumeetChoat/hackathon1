2fe54387b2192dc6324cd464bf2f6de2fc53838c
“SumeetChoat” <sumeetpalchoat@hotmail.co.uk>
2023-06-09 09:33:12 +0100

    added userInput for RPS game

--- hackathon1.js
+++ hackathon1.js
@@ -1,1 +1,17 @@
-//test code hello
+//This game is called Rock, Paper, scissors.
+
+//The possible outcomes are:
+// - Rock beats scissors
+// - Scissors beats paper
+// - Paper beats rock
+// - Tie game = game ends in draw
+
+const getUserChoice = (userInput) => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' | userInput === 'paper' | userInput === 'scissors') {
+        return userInput 
+    } else {
+        console.log('Error, please enter valid response: rock, paper or scossors');
+    }
+}
+
